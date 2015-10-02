b969da3b306d0deb0ead833afda5b8723d5da9b9
Stephanie Wells <steph@strategy11.com>
2015-10-01 20:32:52 -0600

    Reduce duplicate function calls

--- display-widgets.php
+++ display-widgets.php
@@ -114,7 +114,8 @@ class DWPlugin{
 			unset( $term );
 		} else if ( is_post_type_archive() ) {
 			global $wp_query;
-			$type = (get_post_type() === false) ? $wp_query->query["post_type"] : get_post_type();
+			$type = get_post_type();
+			$type = ($type === false) ? $wp_query->query['post_type'] : $type;
 			$show = isset( $instance[ 'type-' . $type . '-archive' ] ) ? $instance[ 'type-' . $type . '-archive' ] : false;
 		} else if ( is_archive() ) {
 			$show = isset( $instance['page-archive'] ) ? $instance['page-archive'] : false;
